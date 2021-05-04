d016d37923fc3c931dd8b4609803c83886721615
Patryk Wolski <pvtryk.w@gmail.com>
2021-05-04 12:18:41 +0200

    init with eureka project

---
+++ eureka/Readme.txt
@@ -0,0 +1,2 @@
+mając otworzony projekt w środowisku intlij należy uruchomić (shift + f10)
+Po uruchomieniu rejestr eureka dostępny jest pod adresem http://localhost:8761
---
+++ eureka/src/test/java/com/example/serviceregistrationanddiscoveryservice/ServiceRegistrationAndDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistrationanddiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationAndDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
