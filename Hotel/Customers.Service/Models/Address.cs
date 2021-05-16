73252b2f11e7147230d692c7c04a58c5d99e6e25
Patryk Wolski <pvtryk.w@gmail.com>
2021-05-16 17:34:01 +0200

    address model

--- Hotel/Customers.Service/Models/Address.cs
+++ Hotel/Customers.Service/Models/Address.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace Customers.Service.Models
 {
     public class Address
     {
+        [BsonElement("Street")]
+        public string Street { get; set; }
+        [BsonElement("ZipCode")]
+        public string ZipCode { get; set; }
+        [BsonElement("City")]
+        public string City { get; set; }
+        [BsonElement("Country")]
+        public string Country { get; set; }
     }
 }
