fd3bd6de648696b9e0e96c8ce013cd4cfa1a62dd
Mandraszewsky <mandrah@o2.pl>
2021-05-20 17:54:44 +0200

    model repaired - adding usings mongoDB

--- Hotel/Reservations.Service/Models/Reservation.cs
+++ Hotel/Reservations.Service/Models/Reservation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
