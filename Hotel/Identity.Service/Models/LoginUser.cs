308617a4f2bfebc835be97158032a1d670261181
Patryk Wolski <pvtryk.w@gmail.com>
2021-05-22 17:08:52 +0200

    login user model

---
+++ Hotel/Identity.Service/Models/LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Models
+{
+    public class LoginUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Hotel/Identity.Service/Services/IdentityService.cs
@@ -0,0 +1,33 @@
+﻿
+using Identity.Service.Extensions;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Identity.Service.DAL;
+using Identity.Service.Models;
+
+namespace Identity.Service.Services
+{
+    public class IdentityService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public IdentityService(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _users = database.GetCollection<User>(settings.CollectionName);
+        }
+
+        public string Login(LoginUser user)
+        {
+
+            return user.Email;
+        }
+    }
+}
